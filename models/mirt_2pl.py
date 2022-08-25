8d5775735f2d68d2b9e210dcbbb7d3239dbbbae0
JesperDannath <39453670+JesperDannath@users.noreply.github.com>
2022-08-25 17:57:06 +0200

    Update MIRT-2PL Model functions

--- models/mirt_2pl.py
+++ models/mirt_2pl.py
@@ -1,10 +1,11 @@
 from irt_model import irt_model
 import numpy as np
+from scipy.stats import multivariate_normal
 
 
 class mirt_2pl(irt_model):
 
-    def __init__(self, item_dimension, latent_dimension, A, delta) -> None:
+    def __init__(self, item_dimension, latent_dimension, A, delta, sigma) -> None:
         """
         Args:
             item_dimension (int): Number of items in test
@@ -15,6 +16,7 @@ class mirt_2pl(irt_model):
         super().__init__(item_dimension, latent_dimension)
         self.item_parameters = {
             "discrimination_matrix": A, "intercept_vector": delta}
+        self.person_parameters = {"covariance": sigma}
 
     def icc(self, theta, A=np.empty(0), delta=np.empty(0)) -> np.array:
         """_summary_
@@ -33,3 +35,19 @@ class mirt_2pl(irt_model):
             np.divide(1, np.add(1, np.exp(np.multiply(-1, linear_predictor)))))
         #p = 1/(1+np.exp(-linear_predictor))
         return(p)
+
+    def joint_competency_answer_density(self, theta, response_vector: np.array, A=np.empty(0),
+                                        delta=np.empty(0), sigma=np.empty(0)) -> np.array:
+        if sigma.size == 0:
+            sigma = self.person_parameters["covariance"]
+        correct_response_probabilities = self.icc(theta, A, delta)[0]
+        marginal_competency_density = multivariate_normal.pdf(x=theta, mean=np.zeros(
+            self.latent_dimension), cov=sigma)
+        joint_density = np.multiply(np.prod(np.multiply(correct_response_probabilities, response_vector)),
+                                    np.prod(np.multiply(np.subtract(1, correct_response_probabilities), np.subtract(1, response_vector))))
+        joint_density = np.multiply(joint_density, marginal_competency_density)
+        return(joint_density)
+
+    def latent_density(self, theta):
+        return(multivariate_normal.pdf(x=theta, mean=np.zeros(
+            self.latent_dimension), cov=self.person_parameters["covariance"]))
