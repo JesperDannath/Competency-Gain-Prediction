76c5997d3bc5279b969a4230535c699e9a068eef
JesperDannath <39453670+JesperDannath@users.noreply.github.com>
2022-08-26 15:16:40 +0200

    Add m_step template

---
+++ em_algorithm/m_step.py
@@ -0,0 +1,7 @@
+class m_step():
+
+    def __init__(self, posterior_expectation_function: function) -> None:
+        self.posterior_expectation_function = posterior_expectation_function
+
+    def step(self) -> None:
+        pass
