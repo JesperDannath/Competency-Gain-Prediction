Various changes and fixes (especially in Simulation of covariance)

em_algorithm/m_step_mirt_2pl.py
@@ -45,7 +45,7 @@ class m_step_ga_mml(m_step):
                 population=list(range(0, len(individual1))), k=int(len(individual1)/2))
             new_individual = np.array([individual2[i] if i in crossover_indices else individual1[i]
                                        for i in range(0, len(individual1))])
-            # TODO: Better hanle constraint case
+            # TODO: Better handle constraint case
             constraint_function(new_individual)
             return(new_individual)
 
@@ -102,27 +102,32 @@ class m_step_ga_mml(m_step):
             iter += 1
         return(population_base[0][1])
 
-    def newton_raphson(self, funct, x0, max_iter=1000):
+    def newton_raphson(self, funct, x0, max_iter=1000, alpha=0.001):
         x_t = x0.copy()
         converged = False
         iter = 0
         while not converged:
+            skip_terminate = False
             x_t_last = x_t.copy()
             first_derivative = funct(x_t)
-            second_derivative = approx_fprime(
-                f=funct, xk=x_t, epsilon=1.4901161193847656e-22).diagonal()
+            second_derivative = np.array(approx_fprime(
+                f=funct, xk=x_t, epsilon=1.4901161193847656e-22).diagonal())
             if 0 in list(second_derivative):
-                first_derivative[second_derivative == 0] = 0 # TODO: Anstatt Nullrunde eher einen kleinen Schritt in die Richtung der First order Ableitung gehen. Learning Rate setzen, Terminierung Aussetzen 
+                # TODO: Anstatt Nullrunde eher einen kleinen Schritt in die Richtung der First order Ableitung gehen. Learning Rate setzen, Terminierung Aussetzen
+                first_derivative[second_derivative == 0] = -1 * alpha * \
+                    first_derivative[second_derivative == 0]
+                second_derivative[second_derivative == 0] = 1
+                skip_terminate = True
             x_t = x_t - np.divide(first_derivative, second_derivative)  # ,
             # out=np.zeros_like(first_derivative), where=second_derivative == 0.0001)
-            if (np.abs(x_t - x_t_last) < 0.1).all() or (iter >= max_iter):
+            if ((np.abs(x_t - x_t_last) < 0.1).all() & (not skip_terminate)) or (iter >= max_iter):
                 converged = True
             iter = iter+1
         return(x_t)
 
     def step(self, pe_functions: dict, person_method="newton_raphson", item_method="ga"):
         # Find the new value for Sigma
-        print("Maximize Q-0")
+        # print("Maximize Q-0")
         # The only parameters we need to optimise are the correlations
         # TODO: Matrix X^tX = sigma benutzen um p.s.d zu enforcen
         log_likelihood = 0.0
@@ -194,6 +199,7 @@ class m_step_ga_mml(m_step):
             elif person_method == "newton_raphson":
                 x0 = scipy.linalg.sqrtm(
                     self.model.person_parameters["covariance"]).flatten()
+                #x0 = scipy.linalg.cholesky(self.model.person_parameters["covariance"]).flatten()
                 new_sigma_sqrt = self.newton_raphson(
                     x0=x0, funct=q_0_gradient_sqrt)
                 new_sigma_sqrt = new_sigma_sqrt.reshape(
@@ -208,7 +214,7 @@ class m_step_ga_mml(m_step):
             shape=self.model.item_parameters["discrimination_matrix"].shape)
         new_delta = np.empty(
             shape=self.model.item_parameters["intercept_vector"].shape)
-        print("Maximize the Q_i's")
+        # print("Maximize the Q_i's")
         for item in range(0, self.model.item_dimension):
             a_init = self.model.item_parameters["discrimination_matrix"][item]
             delta_init = self.model.item_parameters["intercept_vector"][item]
@@ -225,7 +231,8 @@ class m_step_ga_mml(m_step):
 
             # if len(x0) == 1:
             new_item_parameters = self.genetic_algorithm(
-                fitness_function=q_item, x0=x0, constraint_function=lambda arg: np.all(arg[0:len(arg)-1] > 0))
+                fitness_function=q_item, x0=x0, constraint_function=lambda arg: np.all(arg[0:(len(arg)-1)] > 0))
+            # fitness_function=q_item, x0=x0, constraint_function=lambda arg: np.all(arg[0:len(arg)-1] > 0))
             # else:
             #     new_item_parameters = cma.CMAEvolutionStrategy(
             #         x0=x0, sigma0=2).optimize(lambda x: -1*q_item(x), maxfun=1000, n_jobs=0).result.xfavorite

models/mirt_2pl.py
@@ -2,6 +2,7 @@ from irt_model import irt_model
 import numpy as np
 import pandas as pd
 import copy
+import scipy
 from scipy.stats import multivariate_normal
 from scipy.stats.qmc import MultivariateNormalQMC
 from scipy.optimize import minimize
@@ -72,6 +73,7 @@ class mirt_2pl(irt_model):
             print(sigma)
             raise Exception("Covariance is not symmetric")
         if not np.all(np.linalg.eigvals(sigma) >= 0):
+            print(sigma)
             raise Exception("New Covariance not positive semidefinite")
         return(True)
 
@@ -113,11 +115,13 @@ class mirt_2pl(irt_model):
         if not qmc:
             sample = multivariate_normal.rvs(size=sample_size, mean=np.zeros(
                 self.latent_dimension), cov=self.person_parameters["covariance"])
-            if self.latent_dimension == 1:
-                sample = np.expand_dims(sample, axis=1)
+            # if self.latent_dimension == 1:
+            #    sample = np.expand_dims(sample, axis=1)
         else:
             sample = MultivariateNormalQMC(mean=np.zeros(
-                self.latent_dimension), cov=self.person_parameters["covariance"]).random(sample_size)
+                self.latent_dimension), cov=self.person_parameters["covariance"], engine=None).random(sample_size)  # scipy.stats.qmc.Halton(d=self.latent_dimension)).random(sample_size)
+        # Ensure correct dimensionality if sample_siz==1 or latent_dim==1
+        sample = sample.reshape((sample_size, self.latent_dimension))
         return(sample)
 
     def response_matrix_probability(self, theta, response_matrix: np.array, A=np.empty(0),
@@ -218,7 +222,12 @@ class mirt_2pl(irt_model):
         else:
             mask = self.item_parameters["q_matrix"][item].astype(np.bool)
             a_item = np.zeros(mask.shape)
-            np.place(a_item, mask=mask, vals=discriminations)
+            try:
+                np.place(a_item, mask=mask, vals=discriminations)
+            except ValueError:
+                print("mask: {0}".format(mask))
+                print("Discriminations: {0}".format(discriminations))
+                raise Exception("Could not fill zero Discriminations")
             return(a_item)
 
     def marginal_response_loglikelihood(self, response_data, N=1000):
@@ -237,7 +246,7 @@ class mirt_2pl(irt_model):
             (1-answer_vector), np.log(1-ICC_values)[0]) + np.log(latent_density)
         return(log_likelihood)
 
-    def derive_competency(self, response_data: pd.DataFrame) -> np.array:
+    def predict_competency(self, response_data: pd.DataFrame) -> np.array:
         """Given the estimated item-parameters for a MIRT-Model and some response_data, this function will estimate the latent ability for every respondent.
 
         Args:

models/test_mirt_2pl.py
@@ -117,7 +117,7 @@ class test_mirt_2pl(unittest.TestCase):
         self.assertTrue(np.array_equal(new_sigma, np.array(
             [[1, 0.5, 0.1], [0.5, 1, 0.5], [0.1, 0.5, 1]])))
 
-    def test_derive_competency(self):
+    def test_predict_competency(self):
         U = pd.DataFrame(np.array([[1, 1, 1],
                                    [1, 0, 1],
                                    [0, 0, 1]]))

simulation_framework/simulate_competency.py
@@ -14,7 +14,7 @@ class respondent_population():
         self.latent_dimension = latent_dimension
         self.intervention=False
 
-    def initialize_random_person_parameters(self):
+    def initialize_random_person_parameters(self, Q=np.empty(0), q_share=0.0):
         #TODO:  nochmal Q^tQ nehmen und dann den gewichteten Mittelwert bilden!
         #evtl. auch nochmal die Recovery mit unabhängigen Fähigkeiten testen
         X = np.random.rand(self.latent_dimension, self.latent_dimension) #TODO: Um Werte kleiner zu machen Wurzel ziehen
@@ -25,13 +25,22 @@ class respondent_population():
         #     np.dot(inv_sd_matrix, cov), inv_sd_matrix)
         correlation_matrix = make_sparse_spd_matrix(
              dim=self.latent_dimension, alpha=0.2, norm_diag=True, smallest_coef=0.0, largest_coef=0.9)
-        correlation_matrix = np.round(np.abs(correlation_matrix), 4)
         self.latent_distribution = multivariate_normal(mean=np.zeros(self.latent_dimension), cov=correlation_matrix)
-        return(correlation_matrix)
+        correlation_matrix = np.round(np.abs(correlation_matrix), 4)
+        #Calculate COV-Matrix implied by the Q-Matrix
+        q_cov = np.dot(Q.transpose(), Q)
+        q_sd_vector = np.sqrt(q_cov.diagonal())
+        inv_q_sd_matrix = np.linalg.inv(np.diag(q_sd_vector))
+        q_corr = np.dot(
+             np.dot(inv_q_sd_matrix, q_cov), inv_q_sd_matrix)
+        correlation_matrix = np.add((1-q_share)*correlation_matrix, q_share*q_corr)
+        correlation_matrix = np.round(correlation_matrix, 4)
+        # TODO: Use mean for Competency Gain model
+        self.latent_distribution = multivariate_normal(mean=np.zeros(self.latent_dimension), cov=correlation_matrix)
+        return({"covariance": correlation_matrix})
     
     def sample(self, sample_size:int) -> pd.DataFrame:
         """Create a random variable sample from a the defined latent_distribution
-
         Args:
             sample_size (int): _description_
 

simulation_framework/simulate_responses.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+import itertools
 from scipy.stats import bernoulli
 from scipy.stats import multinomial
 from scipy.stats import multivariate_normal
@@ -21,9 +22,11 @@ class response_simulation():
         self.latent_dimension = self.population.latent_dimension
         self.early_model = mirt_2pl(self.item_dimension, self.latent_dimension)
         self.early_model.set_parameters({"item_parameters": early_item_params})
+        if self.item_dimension < self.latent_dimension:
+            raise Exception("To few Items for given latent dimension")
 
     # TODO: Prevent All-zero columns
-    def initialize_random_q_structured_matrix(self, structure="singular"):
+    def initialize_random_q_structured_matrix(self, structure="singular", ensure_id=False):
         if structure == "singular":
             early_Q = np.ones((self.item_dimension, self.latent_dimension))
         elif structure == "seperated":
@@ -40,9 +43,17 @@ class response_simulation():
                         [0 if j != i else 1 for j in range(0, self.latent_dimension)])
             early_Q = np.array(early_Q)
         elif structure == "pyramid":
+            if (self.item_dimension < self.latent_dimension*2) & ensure_id:
+                raise Exception(
+                    "To few Items to ensure Identification for structrue: pyramid")
             # Items mit mehr skills sind ggf. unwahrscheinlicher (linearer abstieg)
             early_Q = []
-            stair_frequencies = multinomial.rvs(self.item_dimension, p=[
+            if ensure_id:
+                early_Q = list(np.identity(self.latent_dimension))
+                pyramid_items = self.item_dimension - self.latent_dimension
+            else:
+                pyramid_items = self. item_dimension
+            stair_frequencies = multinomial.rvs(pyramid_items, p=[
                 1/self.latent_dimension for i in range(0, self.latent_dimension)])
             if 0 in stair_frequencies:
                 self.initialize_random_q_structured_matrix(structure="pyramid")
@@ -54,10 +65,36 @@ class response_simulation():
             early_Q = np.array(early_Q)
         # TODO: Add structure Chained
         # TODO: Add structure Full-Complexity
+        elif (structure == "full"):
+            n_pattern = 2**self.latent_dimension-1  # exclude all zero row
+            if n_pattern > self.item_dimension:
+                raise Exception("To few Items for structure full")
+            pattern_frequencies = multinomial.rvs(self.item_dimension, p=[
+                1/n_pattern for i in range(0, n_pattern)])
+            if 0 in pattern_frequencies:
+                self.initialize_random_q_structured_matrix(structure="full")
+                return
+            patterns = list(itertools.product(
+                [0, 1], repeat=self.latent_dimension))
+            patterns.remove(
+                tuple([0 for i in range(0, self.latent_dimension)]))
+            early_Q = []
+            for i, pattern in enumerate(patterns):
+                for j in range(0, pattern_frequencies[i]):
+                    early_Q.append(pattern)
+            early_Q = np.array(early_Q)
         else:
             raise Exception("Q-Matrix structure not known")
         self.early_model.set_parameters(
             {"item_parameters": {"q_matrix": early_Q, "discrimination_matrix": early_Q}})
+
+    def get_Q(self, time="early"):
+        if time not in ["early", "late"]:
+            raise Exception("Invalid time selected")
+        if time in ["early"]:
+            return self.early_model.item_parameters["q_matrix"]
+        if time in ["late"]:
+            pass
 
     # def initialize_random_item_parameters(self, Q=np.empty(0)):
     #     if Q.size == 0:
@@ -79,7 +116,7 @@ class response_simulation():
         if Q.size == 0:
             Q = self.early_model.item_parameters["q_matrix"]
         # 1. Sample relative difficulties
-        relative_difficulties_sample = np.sqrt(3)*self.population.sample(
+        relative_difficulties_sample = np.sqrt(1)*self.population.sample(
             self.item_dimension)
         # t-Verteilung oder Gleichverteilung mal ausprobieren
         A = np.zeros((self.item_dimension, self.latent_dimension))
@@ -100,11 +137,6 @@ class response_simulation():
         self.early_model.item_parameters.update(item_parameters)
         return(self.early_model.item_parameters)
 
-    # def initialize_random_intercepts(self, sigma):
-    #     #Relative Item-Difficulties will be sampled from the latent distribution
-    #     #Then these will be transformed to the intercept Domain
-    #     relative_difficulties_sample = self.population.sample(self.item_dimension)
-
     def sample(self, sample_size) -> pd.DataFrame:
         sample = {}
         sample["latent_trait"] = self.population.sample(
@@ -117,3 +149,6 @@ class response_simulation():
         sample["latent_dimension"] = self.latent_dimension
         sample["item_dimension"] = self.item_dimension
         return(sample)
+
+    def get_item_parameters(self):
+        return({"real"})